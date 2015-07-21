1a27f7f7da253091f90d1f67aa3869cea677fb61
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-07-22 00:16:44 +0300

    add

--- src/_bionic_university/_5_wikipedia/WikiLinksCollector.java
+++ src/_bionic_university/_5_wikipedia/WikiLinksCollector.java
@@ -12,16 +12,13 @@ public class WikiLinksCollector {
         list = new HashSet<WikiLink>();
     }
 
-    public void addLink(String url, String name) {
+    public int addLink(String url, String name) {
         WikiLink link = new WikiLink(url, name);
         if (list.add(link)) {
             count = link.getId();
         } else
             WikiLink.setCount(count);
-    }
-
-    public void addLink(WikiLink wikiLink) {
-        wikiLink = new WikiLink();
+        return count;
     }
 
     public Set<WikiLink> getList() {
@@ -89,6 +86,8 @@ public class WikiLinksCollector {
             while (true) {
                 WikiLink link = (WikiLink) in.readObject();
                 list.add(link);
+                if (link.getId() > WikiLink.getCount())
+                    WikiLink.setCount(link.getId());
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -98,5 +97,14 @@ public class WikiLinksCollector {
         }
     }
 
+    public void createHtml(String fileName) throws FileNotFoundException {
+        PrintStream outF = new PrintStream(fileName);
+        outF.println("<html><head><title>WikiLinks</title></head><body><ul>");
+        for (WikiLink link : list) {
+            outF.println("<li><a href=\"" + link.getUrl() + "\"" + link.getName() + "</a>");
+        }
+        outF.println("</body></html>");
+        outF.close();
+    }
 
 }
