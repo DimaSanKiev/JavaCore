9f34449e47f96512441ce9f5cc99761c469a5583
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-07-24 11:49:01 +0300

    add

--- src/_bionic_university/_6_cash_management_system/JdbcBasics.java
+++ src/_bionic_university/_6_cash_management_system/JdbcBasics.java
@@ -4,36 +4,60 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.*;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Properties;
 
 public class JdbcBasics {
     public static void main(String[] args) throws IOException, SQLException {
+        preparedInsert();
+    }
+
+    public static void preparedInsert() {
         try {
-            Connection con = DriverManager.getConnection("jdbc:derby:MyDB");
+            Connection con = getConnection();
+            String sql = "INSERT INTO customer (name, address, ";
+            sql += " email, ccNo, ccType, maturity) values(";
+            sql += " 'Clar Nelis', 'Vosselaar st. 19, Trnaut, Belgium', ";
+            sql += " 'Clar@adw.com', 	'11345694671231', ";
+            sql += " 'MasterCard', '2014-07-31') ";
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(
-                    "SELECT name, charge FROM merchant");
-            while (rs.next()) {
-                String nm = rs.getString("name");
-                double p = rs.getDouble(2);
-                System.out.println(nm + "   " + p);
-            }
+            stmt.executeUpdate(sql);
             con.close();
-
-        } catch (SQLException e) {
-            System.out.println("Error " + e.getMessage());
+            System.out.println("New record was inserted");
+        } catch (Exception ex) {
+            System.out.println("Error " + ex.getMessage());
         }
+    }
 
+
+    public static void simpleInsert() {
+        try {
+            LocalDate dt1 = LocalDate.of(2015, 2, 15);
+            Instant instant = dt1.atStartOfDay(ZoneId.systemDefault()).toInstant();
+            java.sql.Date dt = new java.sql.Date(java.util.Date.from(instant).getTime());
+            addCustomer("Willy Nest", "Mountain View, California", "wnest@alpha.com",
+                    "54664423893279", "Visa", dt);
+        } catch (Exception ex) {
+            System.out.println("Error " + ex.getMessage());
+        }
+    }
+
+    public static void addCustomer(String name, String address, String email, String ccNo, String ccType, java.sql.Date dt) throws SQLException, IOException {
         Connection con = getConnection();
         String sql = "INSERT INTO customer (name, address, ";
-        sql += " email, ccNo, ccType, maturity) values(";
-        sql += " 'Clar Nelis', 'Vosselaar st. 19, Trnaut, Belgium', ";
-        sql += " 'Clar@adw.com', 	'11345694671231', ";
-        sql += " 'MasterCard', '2014-07-31') ";
-        Statement stmt = con.createStatement();
-        stmt.executeUpdate(sql);
+        sql += " email, ccNo, ccType, maturity) values(?,?,?,?,?,?) ";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1, name);
+        stmt.setString(2, address);
+        stmt.setString(3, email);
+        stmt.setString(4, ccNo);
+        stmt.setString(5, ccType);
+        stmt.setDate(6, dt);
+        stmt.executeUpdate();
         con.close();
-
+        System.out.println("New record was inserted");
     }
 
     public static Connection getConnection() throws IOException, SQLException {
